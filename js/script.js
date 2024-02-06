8957454d465a675e72404f9659fae3b4e76b7410
CristianEspejel <tapiaxto@gmail.com>
2024-02-06 11:27:27 -0600

    Se agrego carpeta js y script.js al proyecto

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Pedir al usuario que introduzca su nombre
+var nombre = prompt("Por favor, introduce tu nombre:");
+
+// Mostrar un saludo con el nombre introducido
+alert("¡Hola " + nombre + "! Bienvenido.");
+
+// Imprimir el nombre en la consola
+console.log("Nombre introducido:", nombre);
